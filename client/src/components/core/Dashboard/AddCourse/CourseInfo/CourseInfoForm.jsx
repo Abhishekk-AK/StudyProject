6b383ea39d507b984971cb3222c47f87595ae685
Abhishekk-AK <abhishek520abc@gmail.com>
2025-11-08 12:04:31 +0530

    add course info form functionalities added

--- client/src/components/core/Dashboard/AddCourse/CourseInfo/CourseInfoForm.jsx
+++ client/src/components/core/Dashboard/AddCourse/CourseInfo/CourseInfoForm.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
 import { useDispatch, useSelector } from "react-redux"
 import RequirementsField from "./RequirementsField"
-import { setStep } from "../../../../../slices/CourseSlice"
+import { setCourse, setStep } from "../../../../../slices/CourseSlice"
 import IconBtn from "../../../../common/IconBtn"
 import { MdCurrencyRupee, MdNavigateNext } from "react-icons/md"
 import ChipInput from "./ChipInput"
 import Upload from "../Upload"
+import toast from "react-hot-toast"
+import { COURSE_STATUS } from '../../../../../utils/constants'
+import { 
+    addCourseDetails, 
+    editCourseDetails, 
+    fetchCourseCategories 
+} from "../../../../../services/operations/courseApi"
+
 
 const CourseInfoForm = () => {
 
@@ -19,6 +27,121 @@ const CourseInfoForm = () => {
     const [loading, setLoading] = useState(null)
     const [courseCategories, setCourseCategories] = useState([])
     const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        const getCategories = async () => {
+            setLoading(true)
+            const categories = await fetchCourseCategories()
+
+            if(categories.length > 0) {
+                setCourseCategories(categories)
+            }
+            setLoading(false)
+        }
+
+        //if form is in edit mode
+        if(editCourse) {
+            setValue('courseTitle', course.courseName)
+            setValue('courseDesc', course.courseDescription)
+            setValue('coursePrice', course.price)
+            setValue('courseTags', course.tag)
+            setValue('courseBenefits', course.whatYouWillLearn)
+            setValue('courseCategory', course.category)
+            setValue('courseRequirements', course.instructions)
+            setValue('courseImage', course.thumbnail)
+        }
+
+        getCategories()
+        
+    },[])
+
+    //check whether form is updaed or not
+    const isFormUpdated = () => {
+        if(
+            currentValues.courseTitle !== course.courseName 
+            || currentValues.courseDesc !== course.courseDescription
+            || currentValues.coursePrice !== course.price
+            || currentValues.courseTags.toString() !== course.tag
+            || currentValues.courseBenefits !== course.whatYouWillLearn
+            || currentValues.courseCategory._id !== course.category._id
+            || currentValues.courseRequirements.toString() !== course.instructions.toString()
+            || currentValues.courseImage !== course.thumbnail
+        ) {
+            return true
+        }
+        return false
+    }
+
+    //handle next button
+    const onSubmit = async (data) => {
+        
+        if(editCourse) {
+
+            if(isFormUpdated()) {
+                const currentValues = getValues()
+                const formData = new FormData()
+
+                formData.append('courseId', course._id)
+                if(currentValues.courseTitle !== course.courseName)
+                    formData.append('courseName', data.courseTitle)
+
+                if(currentValues.courseDesc !== course.courseDescription)
+                    formData.append('courseDescription', data.courseDesc)
+
+                if(currentValues.coursePrice !== course.price)
+                    formData.append('price', data.coursePrice)
+
+                if(currentValues.courseTags.toString() !== course.tag.toString())
+                    formData.append('tag', JSON.stringify(data.courseTags))
+
+                if(currentValues.courseBenefits !== course.whatYouWillLearn)
+                    formData.append('whatYouWillLearn', data.courseBenefits)
+
+                if(currentValues.courseCategory._id !== course.category._id)
+                    formData.append('category', data.courseCategory)
+
+                if(currentValues.courseRequirements.toString() !== course.instructions.toString())
+                    formData.append('instructions', JSON.stringify(data.courseRequirements))
+
+                if(currentValues.courseImage !== course.thumbnail)
+                    formData.append('thumbnail', data.courseImage)
+
+                setLoading(true)
+                const result = await editCourseDetails(formData, token)
+                setLoading(false)
+
+                if(result) {
+                    dispatch(setStep(2))
+                    dispatch(setCourse(result))
+                }
+            } else {
+                toast.error('No changes made to the form')
+            }
+            return
+        }
+
+        const formData = new FormData()
+
+        formData.append('courseName', data.courseTitle)
+        formData.append('courseDescription', data.courseShortDesc)
+        formData.append('price', data.coursePrice)
+        formData.append('tag', JSON.stringify(data.courseTags))
+        formData.append('whatYouWillLearn', data.courseBenefits)
+        formData.append('category', data.courseCategory)
+        formData.append('status', COURSE_STATUS.DRAFT)
+        formData.append('instructions', JSON.stringify(data.courseRequirements))
+        formData.append('thumbnail', data.courseImage)
+        
+        setLoading(true)
+        const result = await addCourseDetails(formData, token)
+        
+        if (result) {
+        dispatch(setStep(2))
+        dispatch(setCourse(result))
+        }
+        setLoading(false)
+    }
 
   return (
     <>
