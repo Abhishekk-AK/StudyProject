219522a250ea49f633faa5860d8b1dd3dca97c7d
Abhishekk-AK <abhishek520abc@gmail.com>
2025-11-14 15:59:36 +0530

    feat(cart): add addtocart, removefromcart, resetcart in cart  slice

--- client/src/slices/CartSlice.jsx
+++ client/src/slices/CartSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
 
 const initialState = {
     cart: localStorage.getItem('cart')
@@ -16,20 +17,61 @@ const cartSlice = createSlice ({
     name:'cart',
     initialState:initialState,
     reducers: {
-        setCart(state, value) {
-            state.cart = value.payload
-        }, 
-        setTotal(state, value) {
-            state.total = value.payload
-        },
-        setTotalItems(state, value) {
-            state.totalItems = value.payload
-        }
         //add to cart
+        addToCart: (state, action) => {
+            const course = action.payload
+            const index = state.cart.findIndex((item) => item.id === course._id)
+
+            if(index >= 0) {
+                //if the course is already in the cart, do not modify cart
+                toast.message('Course is already in the cart')
+                return
+            }
+
+            //if the course is not in the cart
+            state.cart.push(course)
+            //update quantity and price
+            state.totalItems++
+            state.total += course.price
+
+            localStorage.setItem('cart', JSON.stringify(state.cart))
+            localStorage.setItem('total', JSON.stringify(state.total))
+            localStorage.setItem('totalItems', JSON.stringify(state.totalItems))
+
+            toast.success('Course added to cart')
+        },
+
         //remove from cart
+        removeFromCart: (state, action) => {
+            const courseId = action.payload
+            const index = state.cart.findIndex((item) => item._id === courseId)
+
+            if(index >= 0) {
+                //if course is present remove it
+                state.totalItems--
+                state.total -= state.cart[index].price
+                state.cart.splice(index, 1)
+
+                localStorage.setItem('cart', JSON.stringify(state.cart))
+                localStorage.setItem('total', JSON.stringify(state.total))
+                localStorage.setItem('totalItems', JSON.stringify(state.totalItems))
+
+                toast.success('Course is removed from cart')
+            }
+        },
+
         //reset cart
+        resetCart: (state) => {
+            state.cart = []
+            state.total = 0
+            state.totalItems = 0
+
+            localStorage.removeItem('cart')
+            localStorage.removeItem('total')
+            localStorage.removeItem('totalItems')
+        }
     }
 })
 
-export const {setTotal, setTotalItems} = cartSlice.actions;
+export const { addToCart, removeFromCart, resetCart } = cartSlice.actions;
 export default cartSlice.reducer;
