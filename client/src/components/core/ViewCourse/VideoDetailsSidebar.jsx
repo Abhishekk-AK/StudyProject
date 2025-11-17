3b81aa08e9cf773c5045d222a85da097997c7a8b
Abhishekk-AK <abhishek520abc@gmail.com>
2025-11-17 12:46:32 +0530

    feat(view-course): add video-details sidebar

---
+++ client/src/components/core/ViewCourse/VideoDetailsSidebar.jsx
@@ -0,0 +1,133 @@
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { useLocation, useNavigate, useParams } from "react-router-dom"
+import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai"
+import IconBtn from "../../common/IconBtn"
+
+const VideoDetailsSidebar = ({setReviewModal}) => {
+
+    const navigate = useNavigate()
+    const location = useLocation()
+    const [activeStatus, setActiveStatus] = useState('')
+    const [videobarActive, setVideobarActive] = useState('')
+    const {sectionId, subSectionId} = useParams()
+
+    const {
+        courseSectionData,
+        courseEntireData,
+        totalNoOfLectures,
+        completedLectures
+    } = useSelector((state) => state.viewCourse)
+
+    useEffect(() => {
+        //set active flags
+        ; (() => {
+            if(!courseSectionData.length)
+                return
+            const currentSectionIndex = courseSectionData.findIndex(
+                (data) => data._id === sectionId
+            )
+            const currentSubSectionIndex = courseSectionData?.[currentSectionIndex]?.subSection.findIndex(
+                (data) => data._id === subSectionId
+            )
+            const activeSubSectionId = courseSectionData[currentSectionIndex]?.subSection?.[currentSubSectionIndex]?._id
+            //set current section
+            setActiveStatus(courseSectionData?.[currentSectionIndex]?._id)
+            //set current sub-section
+            setVideobarActive(activeSubSectionId)
+        })()
+    },[courseSectionData, courseEntireData, location.pathname])
+    
+
+  return (
+    <>
+        <div>
+            <div>
+                {/* buttons */}
+                <div>
+                    <div
+                        onClick={() => {
+                            navigate('dashboard/enrolled-courses')
+                        }}
+                    >
+                        Back
+                    </div>
+                    <div>
+                        <IconBtn
+                            text='Add Review'
+                            onclick={() => {
+                                setReviewModal(true)
+                            }}
+                        />
+                    </div>
+                </div>
+                {/* heading and title */}
+                <div>
+                    <p>
+                        {courseEntireData?.courseName}
+                    </p>
+                    <p>
+                        {completedLectures?.length} / {totalNoOfLectures}
+                    </p>
+                </div>
+            </div>
+
+            <div>
+                {
+                    courseSectionData.map((section) => (
+                        <div
+                            key={section._id}
+                            onClick={() => setActiveStatus(section?._id)}
+                        >
+                            <div>
+                                {section?.sectionName}
+                            </div>
+                            <AiOutlineArrowDown/>
+                            <AiOutlineArrowUp/>
+
+                            <div>
+                                {
+                                    section?.subSection.map((topic) => (
+                                        <div
+                                            key={topic._id}
+                                            className={`flex gap-4 p-5
+                                                ${
+                                                    videobarActive === topic._id
+                                                    ? 'bg-yellow-200 text-richblack-900'
+                                                    : 'bg-richblack-900 text-richblack-5'
+                                                }
+                                            `}
+                                            onClick={() => {
+                                                navigate(
+                                                    `/view-course/${courseEntireData?._id}/section/${section._id}/sub-section/${topic._id}`
+                                                )
+                                                setVideobarActive(topic._id)
+                                            }}
+                                        >
+                                            <div>
+                                                <input
+                                                    type="checkbox"
+                                                    checked={completedLectures.includes(topic._id)}
+                                                    onChange={() => {}}
+                                                />
+                                                <span>
+                                                    {topic.title}
+                                                </span>
+                                            </div>
+                                            <span>
+                                                {topic.timeDuration}
+                                            </span>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default VideoDetailsSidebar
