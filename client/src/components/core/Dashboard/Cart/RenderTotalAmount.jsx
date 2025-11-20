4905a14ba4d2756b5770d9e8e1dfe1062634d5b0
Abhishekk-AK <abhishek520abc@gmail.com>
2025-11-20 15:30:14 +0530

    feat(cart): implement successful integration test for buy-course API

--- client/src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ client/src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -1,13 +1,19 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import IconBtn from '../../../common/IconBtn'
+import { buyCourse } from "../../../../services/operations/studentFeaturesApi"
+import { useNavigate } from "react-router-dom"
 
 const RenderTotalAmount = () => {
 
   const {total, cart} = useSelector((state) => state.cart)
+  const {token} = useSelector((state) => state.auth)
+  const {user} = useSelector((state) => state.profile)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleBuyCourse = () => {
     const courses = cart.map((course) => course._id)
-    console.log('bought these courses:', courses)
+    buyCourse(token, courses, user, navigate, dispatch)
   }
 
   return (
