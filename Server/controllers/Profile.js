3a8f158aa09f267bff4f3b6ac1610f1d86ce08ba
Abhishekk-AK <abhishek520abc@gmail.com>
2025-11-22 15:45:12 +0530

    feat(profile): update update-profile api backend

--- Server/controllers/Profile.js
+++ Server/controllers/Profile.js
@@ -1,27 +1,37 @@
 const Profile = require('../models/Profile');
 const User = require('../models/User');
-const Course = require('../models/Course')
 
 exports.updateProfile = async (req, res) => {
     try {
         //get data
-        const {dateOfBirth='', about='', contactNumber, gender} = req.body;
+        const {
+            firstName='',
+            lastName='',
+            dateOfBirth='',
+            about='',
+            contactNumber,
+            gender
+        } = req.body;
 
         //get userId
         const id = req.user.id;
-
-        //data validation
-        if(!contactNumber || !gender || !id) {
-            return res.status(400).json({
-                success:false,
-                message:'All fields are required.'
-            })
-        }
 
         //find profile
         const userDetails = await User.findById(id);
         const profileId = userDetails.additionalDetails;
         const profileDetails = await Profile.findById(profileId);
+
+        //update user
+        const user = await User.findByIdAndUpdate(
+                                                    id,
+                                                    {
+                                                        firstName,
+                                                        lastName
+                                                    },
+                                                    {new:true}
+                                                )
+
+        await user.save()
 
         //update profile
         profileDetails.dateOfBirth = dateOfBirth;
@@ -31,11 +41,13 @@ exports.updateProfile = async (req, res) => {
 
         await profileDetails.save();
 
+        const updatedUser = await User.findById(id).populate('additionalDetails')
+
         //return response
         return res.status(200).json({
             success:true,
             message:'Profile details updated successfully.',
-            profileDetails
+            data:updatedUser
         })
 
     } catch (err) {
@@ -90,7 +102,7 @@ exports.deleteAccount = async (req, res) => {
         //delete profile
         await Profile.findByIdAndDelete({_id:userDetails.additionalDetails});
 
-        //unenroll user from all courses 
+        //unenroll user from all courses
 
         //delete user
         await User.findByIdAndDelete({_id:id});
